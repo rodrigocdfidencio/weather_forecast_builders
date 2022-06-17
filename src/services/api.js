2de6090db32b032a18c14c4501cd40c3e730da26
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2022-06-17 17:20:16 -0300

    Updating files for HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const OPEN_WEATHER_KEY = process.env.REACT_APP_OPEN_WHEATHER_KEY;
   
 export default axios.create({
-  baseURL: `http://api.openweathermap.org/data/2.5/weather?appid=${OPEN_WEATHER_KEY}&`,
+  baseURL: `https://api.openweathermap.org/data/2.5/weather?appid=${OPEN_WEATHER_KEY}&`,
   params: {
     units: 'metric',
     lang: 'pt_br',
